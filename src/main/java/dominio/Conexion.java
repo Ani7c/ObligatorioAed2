4a8e08cc4a47b94a6d9c391bfe64d8accd2a9a8f
Nicobols0 <nicohermida15@gmail.com>
2024-10-30 20:12:27 -0300

    Clase sucursal y conexion completa

---
+++ src/main/java/dominio/Conexion.java
@@ -0,0 +1,43 @@
+package dominio;
+
+public class Conexion {
+    private int latencia;
+    private boolean existe;
+
+    public Conexion() {
+        this.existe = false;
+    }
+
+    public Conexion(int latencia) {
+        this.latencia = latencia;
+        this.existe = true;
+    }
+    public Conexion(Conexion c) {
+        this.latencia = c.getLatencia();
+        this.existe = c.isExiste();
+    }
+    public int getLatencia() {
+
+        return latencia;
+    }
+
+    public void setLatencia(int latencia) {
+        this.latencia = latencia;
+    }
+
+    public boolean isExiste() {
+        return existe;
+    }
+
+    public void setExiste(boolean existe) {
+        this.existe = existe;
+    }
+
+    @Override
+    public String toString() {
+        return "Conexion{" +
+                "latencia=" + latencia +
+                ", existe=" + existe +
+                '}';
+    }
+}
